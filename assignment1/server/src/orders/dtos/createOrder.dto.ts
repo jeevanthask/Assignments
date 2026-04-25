4cb427998572dab9b98c82a5ed697e31be51c17d
jeevanthask <jeevanthanandika@gmail.com>
2026-04-25 08:00:52 +0530

    table update done

--- assignment1/server/src/orders/dtos/createOrder.dto.ts
+++ assignment1/server/src/orders/dtos/createOrder.dto.ts
@@ -1,9 +1,19 @@
+import { IsEnum, IsInt, IsString } from 'class-validator';
 import { OrderStatus } from '../enums/orderStatus.enum';
 
 export class CreateOrderDTO {
+  @IsInt()
   userId: number;
+
+  @IsString()
   productName: string;
+
+  @IsInt()
   quantity: number;
+
+  @IsInt()
   totalAmount: number;
+
+  @IsEnum(OrderStatus)
   status: OrderStatus;
 }
