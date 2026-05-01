bc2daebbade0c4a7d4be29278f7f428bf24518b0
jeevanthask <jeevanthanandika@gmail.com>
2026-05-01 08:45:12 +0530

    config files added

--- assignment1/server/src/app.module.ts
+++ assignment1/server/src/app.module.ts
@@ -6,22 +6,43 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './orders/entities/order.entity';
 import { KafkaModule } from './kafka/kafka.module';
 import { NotificationModule } from './notification/notification.module';
+import { BullModule } from '@nestjs/bull';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import * as path from 'path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(process.cwd(), '.env'),
+    }),
     OrdersModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'assignment1db',
-      entities: [Order],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DATABASE_HOST'),
+        port: config.get<number>('DATABASE_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DATABASE'),
+        entities: [Order],
+        synchronize: true,
+      }),
     }),
     KafkaModule,
     NotificationModule,
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        redis: {
+          host: config.get<string>('BULL_HOST'),
+          port: config.get<number>('BULL_PORT'),
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- assignment1/server/src/notification/notification.module.ts
+++ assignment1/server/src/notification/notification.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { BullModule } from '@nestjs/bull';
+import { NotificationProcessor } from './notification.processor';
 
 @Module({
-  providers: [NotificationService],
+  imports: [
+    BullModule.registerQueue({
+      name: 'notification-queue',
+    }),
+  ],
+  providers: [NotificationService, NotificationProcessor],
   controllers: [NotificationController],
   exports: [NotificationService],
 })
